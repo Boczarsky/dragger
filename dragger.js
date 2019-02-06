0ac490fa7e5ccf291bdd13bc2781a558f5b15761
Boczarsky <45633311+Boczarsky@users.noreply.github.com>
2019-02-06 10:15:22 +0100

    Create dragger.js

---
+++ dragger.js
@@ -0,0 +1,33 @@
+class Dragger {
+    constructor() {
+        this.target = null;
+        document.addEventListener('mousemove', this.drag);
+        document.addEventListener('mouseup', this.stopDragging)
+    }
+    trackPosition = (event) => {
+        this.mouseX = event.pageX;
+        this.mouseY = event.pageY;
+    }
+    move = () => {
+        const draggableComponent = this.target.parentElement;
+        draggableComponent.style.transform = `translate(${this.mouseX - this.offsetX}px,${this.mouseY - this.offsetY}px)`;
+    }
+    drag = (event) => {
+        this.trackPosition(event);
+        if(this.target) {
+            event.preventDefault();
+            this.move();
+        }
+    }
+    addHandle = (element) => {
+        element.addEventListener('mousedown', this.startDragging);
+    }
+    startDragging = (event) => {
+        this.offsetX = event.offsetX;
+        this.offsetY = event.offsetY;
+        this.target = event.target;
+    }
+    stopDragging = () => {
+        this.target = null;
+    }
+}
